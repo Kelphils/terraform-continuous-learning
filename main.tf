75de0399661bfe76de0b7ae37425ade449ede78b
Kelvin Obioha <obiohakelvin96@gmail.com>
2022-07-10 23:49:14 +0100

    s3 bucket for terraform state, locking and terraform backend

--- main.tf
+++ main.tf
@@ -171,3 +171,76 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
+# s3 bucket to store terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bucket-tutorials"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Enable versioning so you can see the full revision history of your
+# state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# DynamoDb locking by creating DynamoDb table for storing state with primary key of "LockID" to prevent concurrent runs
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-learning-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+# backend configuration for the terraform state in S3 bucket with the DynamoDb table as the backend and encryption, locking enabled
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-state-bucket-tutorials"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-learning-locks"
+    encrypt        = true
+  }
+}
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
